1f29faa9c307398800b3282a23f31ca771c0db74
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-09-08 16:57:11 +0100

    Use the correct pointer for provenance
    Use the source pointer for provenance when setting a parent pointer.

--- external/tree-macros/include/sys/tree.h
+++ external/tree-macros/include/sys/tree.h
@@ -348,8 +348,8 @@ struct {								\
 #define RB_EMPTY(head)			(RB_ROOT(head) == NULL)
 
 #define RB_SET_PARENT(dst, src, field) do {				\
-	RB_BITS(dst, field) &= RB_RED_MASK;				\
-	RB_BITS(dst, field) |= (__uintptr_t)src;			\
+	RB_BITS(dst, field) = (__uintptr_t)src |			\
+	    (ptraddr_t)(RB_BITS(dst, field) & RB_RED_MASK);		\
 } while (/*CONSTCOND*/ 0)
 
 #define RB_SET(elm, parent, field) do {					\
